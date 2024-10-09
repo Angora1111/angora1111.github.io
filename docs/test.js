94dbbe0394a9b485ee654a69928d77c40c2876b1
Angora1111 <m0a221047e@edu.teu.ac.jp>
2024-10-09 18:21:12 +0900

    出力する内容を追加

--- docs/test.js
+++ docs/test.js
@@ -1,2 +1,19 @@
-console.log("こちらは.jsファイルから実行しています。")
-console.log("＜聞こえてますかー！")
+console.log("こちらは.jsファイルから実行しています。");
+console.log("＜聞こえてますかー！");
+
+// 基本の使い方
+let variableA; // 宣言
+variableA = "最初の代入A"; // 代入
+console.log(variableA); // 参照
+
+// 宣言と代入を同時にできる
+let variableB = "最初の代入B";
+console.log(variableB);
+
+// letで宣言した変数の中身は代入で上書きできる
+variableA = "二回目の代入A";
+console.log(variableA);
+
+// 変数に別の変数の値を代入することもできる
+variableA = variableB;
+console.log(variableA);
